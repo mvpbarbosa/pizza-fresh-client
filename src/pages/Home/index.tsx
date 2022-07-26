475dc3f6197c8a773acaca444db7fc2b148c6b06
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-07-26 20:24:45 -0300

    initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,40 @@
+import Menu from "components/Menu";
+import * as S from "./style";
+import { ReactComponent as Search } from "assets/icons/search.svg";
+
+const Home = () => {
+  return (
+    <S.Home>
+      <Menu />
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Detalhes dos pedidos aqui</p>
+      </aside>
+    </S.Home>
+  );
+};
+
+export default Home;
