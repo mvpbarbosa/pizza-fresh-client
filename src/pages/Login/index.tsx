acb44b987b1c074ced98eac6d0ef2d016ca83f33
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-07-26 21:26:44 -0300

    feat(login): Implement  component

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./style"
+
+const Login = () => {
+    return(
+        <S.Login>
+            <S.LoginContent>
+                
+            </S.LoginContent>
+        </S.Login>
+    )
+}
+
+export default Login
