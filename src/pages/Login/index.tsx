e8102bc6a1f8fd4bf94cbb5d2967c0d0ae673b97
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-07-26 21:51:17 -0300

    feat(login): Add `BoxLogin` component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,11 @@
+import BoxLogin from "components/BoxLogin"
 import * as S from "./style"
 
 const Login = () => {
     return(
         <S.Login>
             <S.LoginContent>
-                
+                <BoxLogin />
             </S.LoginContent>
         </S.Login>
     )
