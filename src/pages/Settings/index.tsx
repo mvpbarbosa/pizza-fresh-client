0a72e85bcab81150847935a4b537734343c73766
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-08-09 20:13:24 -0300

    feat: implement `Settings` page

---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,31 @@
+import Menu from "components/Menu";
+import { navigationItems } from "data/navigation";
+import { Outlet } from "react-router-dom";
+import { RoutePath } from "types/routes";
+import * as S from "./style";
+
+const Settings = () => {
+  return (
+    <S.Settings>
+      <Menu active={RoutePath.SETTINGS} navItems={navigationItems} />
+      <S.SettingsPage>
+        <header>
+          <S.SettingsPageHeaderTitle>Configurações</S.SettingsPageHeaderTitle>
+        </header>
+        <S.SettingsContent>
+          <S.SettingsContentSidebar>
+            {"Componente NavColumn"}
+          </S.SettingsContentSidebar>
+          <S.SettingsContentBox>
+            <S.SettingsContentBoxEmpty>
+              Selecione uma categoria
+            </S.SettingsContentBoxEmpty>
+            <Outlet />
+          </S.SettingsContentBox>
+        </S.SettingsContent>
+      </S.SettingsPage>
+    </S.Settings>
+  );
+};
+
+export default Settings;
