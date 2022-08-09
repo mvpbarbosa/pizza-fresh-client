182cf30d6a5d2100dd441f2656432e7d9689c077
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-08-09 20:41:54 -0300

    feat: implement `ManageProducts` component

--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,15 @@ import { RoutePath } from 'types/routes';
 import Home from "pages/Home/index";
 import Login from 'pages/Login';
 import Settings from 'pages/Settings';
+import ManageProducts from 'components/ManageProducts';
 
 const Router = () => {
     return (
         <Routes>
             <Route path={RoutePath.LOGIN} element={<Login />} />
-            <Route path={RoutePath.HOME} element={<Settings />} />
+            <Route path={RoutePath.HOME} element={<Home />} />
+            <Route path={RoutePath.SETTINGS} element={<Settings />} />
+            <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
         </Routes>
     );
 }
