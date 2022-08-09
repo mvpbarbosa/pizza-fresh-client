a8e1afd42479f00cf317e052c0d2ba225d35a4b9
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-08-09 20:46:06 -0300

    feat: implement `ManageUsers` component

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import { RoutePath } from 'types/routes';
+import { Routes, Route } from "react-router-dom";
+import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
-import Login from 'pages/Login';
-import Settings from 'pages/Settings';
-import ManageProducts from 'components/ManageProducts';
+import Login from "pages/Login";
+import Settings from "pages/Settings";
+import ManageProducts from "components/ManageProducts";
+import ManageUsers from "components/ManageUsers";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path={RoutePath.LOGIN} element={<Login />} />
-            <Route path={RoutePath.HOME} element={<Home />} />
-            <Route path={RoutePath.SETTINGS} element={<Settings />} />
-            <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path={RoutePath.LOGIN} element={<Login />} />
+      <Route path={RoutePath.HOME} element={<Home />} />
+      <Route path={RoutePath.SETTINGS} element={<Settings />} />
+      <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
+      <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+    </Routes>
+  );
+};
 
 export default Router;
