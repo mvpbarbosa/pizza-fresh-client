aaa56ea037d306dbfb5ea04f1ee471b8e6ec4901
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-08-09 21:35:08 -0300

    chore: Configuring Login to Home navigation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,24 @@
-import BoxLogin from "components/BoxLogin"
-import * as S from "./style"
+import BoxLogin from "components/BoxLogin";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { RoutePath } from "types/routes";
+import * as S from "./style";
 
 const Login = () => {
-    return(
-        <S.Login>
-            <S.LoginContent>
-                <BoxLogin />
-            </S.LoginContent>
-        </S.Login>
-    )
-}
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = () => {
+    navigate(RoutePath.HOME);
+  };
+
+  return (
+    <S.Login>
+      <S.LoginContent>
+        <BoxLogin onSubmitData={handleSubmit} errorMessage={errorMessage} />
+      </S.LoginContent>
+    </S.Login>
+  );
+};
+
+export default Login;
