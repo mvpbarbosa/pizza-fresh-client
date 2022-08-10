6b429cbe9c6edfd7571abd1c1a24a556a219b998
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-08-09 21:28:25 -0300

    refact: Routes

--- src/router.tsx
+++ src/router.tsx
@@ -2,18 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
 import Login from "pages/Login";
-import Settings from "pages/Settings";
 import ManageProducts from "components/ManageProducts";
 import ManageUsers from "components/ManageUsers";
+import Settings from "pages/Settings";
 
 const Router = () => {
   return (
     <Routes>
       <Route path={RoutePath.LOGIN} element={<Login />} />
       <Route path={RoutePath.HOME} element={<Home />} />
-      <Route path={RoutePath.SETTINGS} element={<Settings />} />
-      <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
-      <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+      <Route path={RoutePath.SETTINGS} element={<Settings />}>
+        <Route
+          path={RoutePath.SETTINGS_PRODUCTS}
+          element={<ManageProducts />}
+        />
+        <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+      </Route>
     </Routes>
   );
 };
