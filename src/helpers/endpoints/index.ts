e0a067a5a2eda8e4c4ae384103a5cff34ac481c6
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-08-30 20:30:30 -0300

    feat: API endpoint preparation

---
+++ src/helpers/endpoints/index.ts
@@ -0,0 +1,14 @@
+import { auth } from "./auth";
+import { order } from "./order";
+import { product } from "./product";
+import { table } from "./table";
+import { user } from "./user";
+
+export const endpoint = {
+  baseUrl: "https://pizza-fresh-server.herokuapp.com",
+  ...auth,
+  ...user,
+  ...order,
+  ...table,
+  ...product,
+};
